fd614356516fe71878660713272ac67570b54dc3
mzdv <mzdv@outook.com>
2014-10-30 19:01:10 +0100

    Added pretty print.

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ var server = http.createServer(function (request, response) {
 
 	// console.log(request.headers.host);
 
-	response.write(request.headers.host);
-	response.write(request.headers['user-agent']);	//dirty
-	response.write(request.url);
-	response.end(request.method);
+	response.write("Host: " + request.headers.host + '\n');
+	response.write("User-agent: " + request.headers['user-agent'] + '\n');	//dirty
+	response.write("HTTP method: " + request.method + '\n');
+	response.end("Request: " + request.url + '\n');
 
 })
 
 .on('error', function(error) {
-	console.log("Error log: " + error)
+	console.log("Error log: " + error + '\n')
 })
 
 .listen("1337", "127.0.0.1");
